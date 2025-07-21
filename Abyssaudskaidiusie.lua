ae2caab18574e89fe568b7438b5eef403e1edeb6
Claire <fgbffdfbgff@gmail.com>
2025-07-21 10:28:49 +0700

    Update Abyssaudskaidiusie.lua

--- Abyssaudskaidiusie.lua
+++ Abyssaudskaidiusie.lua
@@ -1,5 +1,5 @@
-local redtweeninfo = TweenInfo.new(8)
-local redinfo = {Color = Color3.new(1, 1, 1)}
+local redtweeninfo = TweenInfo.new(10)
+local redinfo = {Color = Color3.new(0, 0.5, 1)}
 ----------
 for i,v in pairs(workspace.CurrentRooms:FindFirstChild(game.ReplicatedStorage.GameData.LatestRoom.Value):GetDescendants()) do
     if v:IsA("Light") then
@@ -36,7 +36,7 @@ Bubble.Lifetime = NumberRange.new(1, 10)
 Bubble.Size = NumberSequence.new(20)
 Bubble.Brightness = 20
 
-entity.Rush.PlaybackSpeed = 0.05
+entity.Rush.PlaybackSpeed = 0.04
 entity.Rush.Volume = 5
 entity.Rush.MaxDistance = 150
 
@@ -44,14 +44,17 @@ entity.sound.PlaybackSpeed = 0.03
 entity.sound.Volume = 5
 entity.sound.MaxDistance = 150
 
+entity.Rush:Stop()
+entity.sound:Stop()
+
 task.wait(4)
 task.spawn(function()
-while true do wait(0.1)
+while true do wait(1)
 local ray = game.Workspace:Raycast(entity.Position,game.Players.LocalPlayer.Character.HumanoidRootPart.Position - entity.Position)
         if ray.Instance.Parent == game.Players.LocalPlayer.Character or ray.Instance.Parent.Parent == game.Players.LocalPlayer.Character then
         if dmg == true then
         if not chr:GetAttribute("Hiding") then
-           game.Players.LocalPlayer.Character.Humanoid.Health -= 3
+           game.Players.LocalPlayer.Character.Humanoid.Health -= 5
            game:GetService("ReplicatedStorage").GameStats["Player_".. game.Players.LocalPlayer.Name].Total.DeathCause.Value = "Abyss"
         end
         end
@@ -68,7 +71,7 @@ task.spawn(function()
 			local hum = char and char:FindFirstChildOfClass("Humanoid")
 			local hrp = char and char:FindFirstChild("HumanoidRootPart")
 			
-			if hum and hrp and (hrp.Position - entity.Position).Magnitude <= 7 then
+			if hum and hrp and (hrp.Position - entity.Position).Magnitude <= 10 then
 			if not chr:GetAttribute("Hiding") then
 	            game.Players.LocalPlayer.Character.Humanoid.Health -= 50
                 game:GetService("ReplicatedStorage").GameStats["Player_".. game.Players.LocalPlayer.Name].Total.DeathCause.Value = "Abyss"
@@ -85,14 +88,20 @@ local rooms = workspace.CurrentRooms
 local a = rooms:FindFirstChild(game.ReplicatedStorage.GameData.LatestRoom.Value + 1)
 local b = rooms:FindFirstChild(game.ReplicatedStorage.GameData.LatestRoom.Value)
 local c = rooms:FindFirstChild(game.ReplicatedStorage.GameData.LatestRoom.Value - 1)
+local d = rooms:FindFirstChild(game.ReplicatedStorage.GameData.LatestRoom.Value - 2)
 
-entity.CFrame = a.RoomEntrance.CFrame * CFrame.new(0,3,0)
-local to = game.TweenService:Create(entity, TweenInfo.new(math.random(5, 10), Enum.EasingStyle.Linear, Enum.EasingDirection.Out), {Position = b.RoomEntrance.Position + Vector3.new(0, 3, 0)})
+entity.CFrame = a.RoomEntrance.CFrame * CFrame.new(0,3,-5)
+entity.Rush:Play()
+entity.sound:Play()
+local to = game.TweenService:Create(entity, TweenInfo.new(10, Enum.EasingStyle.Linear, Enum.EasingDirection.Out), {Position = b.RoomEntrance.Position + Vector3.new(0, 2, 0)})
           to:Play()
           to.Completed:Wait()
-local tso = game.TweenService:Create(entity, TweenInfo.new(math.random(5, 10), Enum.EasingStyle.Linear, Enum.EasingDirection.Out), {Position = c.RoomEntrance.Position + Vector3.new(0, 3, 0)})
+local tso = game.TweenService:Create(entity, TweenInfo.new(7, Enum.EasingStyle.Linear, Enum.EasingDirection.Out), {Position = c.RoomEntrance.Position + Vector3.new(0, 2, 0)})
           tso:Play()
           tso.Completed:Wait()
+local hehe = game.TweenService:Create(entity, TweenInfo.new(5, Enum.EasingStyle.Linear, Enum.EasingDirection.Out), {Position = d.RoomEntrance.Position + Vector3.new(0, 2, 0)})
+          hehe:Play()
+          hehe.Completed:Wait()
 dmg = false
 entity.Rush:Stop()
 entity.sound:Stop()
